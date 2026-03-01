3a85be3e211083a87723534b8cc46aba54e4482c
Jinzukai <quochung321123@gmail.com>
2026-03-01 23:49:01 +0700

    Update Sunflower: add pause check, resume timer, and stop method

--- src/Sunflower.java
+++ src/Sunflower.java
@@ -1,21 +1,42 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
-/**
- * Created by Armin on 6/28/2016.
- */
 public class Sunflower extends Plant {
 
     private Timer sunProduceTimer;
 
     public Sunflower(GamePanel parent, int x, int y) {
         super(parent, x, y);
-        sunProduceTimer = new Timer(15000, (ActionEvent e) -> {
-            Sun sta = new Sun(getGp(), 60 + x * 100, 110 + y * 120, 130 + y * 120);
-            getGp().getActiveSuns().add(sta);
-            getGp().add(sta, new Integer(1));
-        });
+
+        sunProduceTimer = new Timer(12000, (ActionEvent e) -> produceSun());
         sunProduceTimer.start();
     }
 
+    private void produceSun() {
+
+        if (getGp().isPaused() || getGp().isLevelFinished() || getGp().isGameOver()) {
+            return;
+        }
+
+        int pixelX = 60 + getX() * 100;
+        int pixelY = 110 + getY() * 120;
+
+        Sun sun = new Sun(getGp(), pixelX, pixelY, pixelY + 20);
+
+        getGp().getActiveSuns().add(sun);
+        getGp().add(sun, 1);
+    }
+
+    @Override
+    public void stop() {
+        if (sunProduceTimer != null) {
+            sunProduceTimer.stop();
+        }
+    }
+    
+    public void resume() {
+        if (sunProduceTimer != null && !sunProduceTimer.isRunning()) {
+            sunProduceTimer.start();
+        }
+    }
 }
